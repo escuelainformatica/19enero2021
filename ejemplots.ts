3a0b93b8c782e38aeb0c1ce6f0cb0ee226166a02
Jorge Castro <jorge.castro.c@gmail.com>
2021-01-19 20:48:54 -0300

    primera version

---
+++ ejemplots.ts
@@ -0,0 +1,14 @@
+// typescript: un lenguaje.
+// transpiler. typescript->javascript
+
+class Cliente {
+    rut:string;
+    nombre:string;
+    edad:bigint;
+}
+
+var john=new Cliente();
+
+john.rut="1-2";
+//john.edad="aaa";
+console.log(john);
